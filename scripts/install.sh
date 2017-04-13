42b83b96a4c4b655bd9843b19d3b920168f736c0
Adrian <adimandevelopment@gmail.com>
2017-04-13 19:10:29 +0000

    improve install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git checkout .zshrc
-source .zshrc
+vim -c VundleUpdate -c quitall
+./zsh-install.sh
