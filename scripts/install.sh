ba6901c9af42a440ea39b4a3f98c4468817fb43e
Adrian Henry <adrian@headspin.io>
2018-04-20 17:21:00 +0000

    install pyenv as part of the install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 vim -c VundleUpdate -c quitall
 ./zsh-install.sh
