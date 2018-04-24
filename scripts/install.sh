ebf2a0bca33222e646a3ed31eff11b34a9e5a630
Adrian Henry <adrian@hungryturtlecode.com>
2018-04-24 08:48:08 +0100

    add support for rbenv in install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 vim -c VundleUpdate -c quitall
 ./zsh-install.sh
