bd9cfb6e7399559584b46e311445b22b74a7faee
Adrian Henry <adrian@hungryturtlecode.com>
2018-04-24 08:53:56 +0100

    add nvm to install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,5 +2,6 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.9/install.sh | bash
 vim -c VundleUpdate -c quitall
-./zsh-install.sh
+~/scripts/zsh-install.sh
